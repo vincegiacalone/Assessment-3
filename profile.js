65542736a2b6a0cbd894efffada62ce6b00a8cb4
Vincent Giacalone <vgiacalone89@gmail.com>
2024-02-20 15:54:59 -0500

    updated

--- profile.js
+++ profile.js
@@ -2,20 +2,20 @@ const colorButton = document.getElementById('color');
 const placeButton = document.getElementById('place');
 const ritualButton = document.getElementById('ritual');
 
-// Event listener for the "My Favorite Color" button
+
 colorButton.addEventListener('click', function() {
-    // Alert the user of your favorite color
+    
     alert("My favorite color is purple.");
 });
 
-// Event listener for the "My Favorite Place" button
+
 placeButton.addEventListener('click', function() {
-    // Alert the user of your favorite place
+    
     alert("My favorite place is in a hammock on the mountains.");
 });
 
-// Event listener for the "My Favorite Ritual" button
+
 ritualButton.addEventListener('click', function() {
-    // Alert the user of your favorite ritual
+    
     alert("My favorite ritual is gaming at night with my friends!");
 });
